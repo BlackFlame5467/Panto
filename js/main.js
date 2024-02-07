99a74b02a0b12b49437b444adae15857fbbd44d3
BlackFlame <blackflame5467@gmail.com>
2024-02-07 11:57:17 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@ const infoBtns = document.querySelectorAll('.info__dot');
 const infoHints = document.querySelectorAll('.info__hint');
 const tabBtns = document.querySelectorAll('[data-tab]');
 const tabCards = document.querySelectorAll('[data-tab-value]');
-
-console.log(tabCards);
+const navBtn = document.querySelector('.nav__btn');
+const navBtnClose = document.querySelector('.mobile-nav__btn-close');
+const mobileNav = document.querySelector('.mobile-nav__wrapper');
 
 infoBtns.forEach(function(element) {
     element.addEventListener('click', function(e){
         e.stopPropagation();
+        infoHints.forEach(function(element) {
+            element.classList.add('none');
+        });
         this.parentNode.querySelector('.info__hint').classList.toggle('none');
     });
 });
@@ -29,9 +33,27 @@ const swiper = new Swiper('.swiper', {
     // direction: 'vertical',
     // loop: true,
   
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 42,
     freeMode: true,
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+
+        920: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1230: {
+            slidesPerView: 4,
+            spaceBetween: 42,
+        }
+    },
+
+
 
     // Navigation arrows
     navigation: {
@@ -46,12 +68,12 @@ tabBtns.forEach(function(e) {
         if(this.classList.value === 'products__tab') {
             tabBtns.forEach(function(element) {
                 element.classList.remove('products__tab--active');
-            })
+            });
 
             this.classList.add('products__tab--active');
         } else {
             this.classList.remove('products__tab--active');
-        }
+        };
 
         for (let card of tabCards) {
 
@@ -60,8 +82,14 @@ tabBtns.forEach(function(e) {
             if((card.dataset.tabValue === this.dataset.tab) || (this.classList.value === 'products__tab')) {
                 card.classList.remove('none');
             }
-        }
+        };
 
         swiper.update();
     })
-})
+});
+
+
+navBtn.addEventListener('click', () => {
+    mobileNav.classList.add("mobile-nav__wrapper--open");
+});
+navBtnClose.addEventListener('click', () => mobileNav.classList.remove("mobile-nav__wrapper--open"))
